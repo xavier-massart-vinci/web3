da9ea443ea1978200f6a53a813191eb57f9fb843
xavier-massart-vinci <xavier.massart@student.vinci.be>
2024-10-01 16:26:31 +0200

    module3 : loading

--- exercises/modules3/3.1/src/components/App/App.jsx
+++ exercises/modules3/3.1/src/components/App/App.jsx
@@ -1,14 +1,21 @@
 import Statistics from 'components/Statistics/Statistics'
 import Button from 'components/Button/Button'
 import { useState } from 'react'
+import Loading from 'components/Loading/Loading'
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [loading, setLoading] = useState(true);
 
 
+  setTimeout(() => {
+    setLoading(false);
+  }, 3000);
+
+  if(loading) return <Loading/>;
 
   return (
     <>
