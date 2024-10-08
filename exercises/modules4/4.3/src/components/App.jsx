84ed0e814108fb62fcce102f10a8a1c57d40bd63
xavier-massart-vinci <xavier.massart@student.vinci.be>
2024-10-08 15:27:07 +0200

    modules4: 4.3

---
+++ exercises/modules4/4.3/src/components/App.jsx
@@ -0,0 +1,70 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('');
+
+  const hook = () =>{
+    axios.
+    get("http://localhost:3001/persons")
+    .then(response => {
+      setPersons(response.data)
+    })
+  }
+ 
+  useEffect(hook, []);
+
+  const addPhone = (e) =>{
+    e.preventDefault();
+
+    if(persons.filter(p => p.name === newName).length != 0){
+      alert(`${newName} is already added to phonebook`);
+    }else{
+      const person = {
+        name: newName,
+        number: newNumber
+      }
+      axios
+      .post("http://localhost:3001/persons", person)
+      .then(result => {
+        setPersons(persons.concat(result.data)) 
+      })
+    }
+    setNewName('');
+    setNewNumber('');
+  }
+
+  const handleNameChange = (e) =>{
+    setNewName(e.target.value);
+  }
+
+  const handleNumberChange = (e) =>{
+    setNewNumber(e.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPhone}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((persone) => <li key={persone.name}>{persone.name} | {persone.number}</li>)}
+      </ul>
+      <div>debug: {newName} | {newNumber}</div>
+    </div>
+  )
+}
+
+export default App
